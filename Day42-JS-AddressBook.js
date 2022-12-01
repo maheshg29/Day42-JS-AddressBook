e92ecf191d7a2582e7ce7bb9b55f73a145d24b40
mahesh <gangurdemahesh9@gmail.com>
2022-12-01 16:45:42 +0530

    [mahesh]add UC1 File

--- Day42-JS-AddressBook.js
+++ Day42-JS-AddressBook.js
@@ -11,4 +11,15 @@ class Contact
     PhoneNumber;
     Email;
 
+    constructor(...param)
+    {
+        this.FirstName = param[0];
+        this.LastName = param[1];
+        this.Address = param[2];
+        this.City = param[3];
+        this.State = param[4];
+        this.Zip = param[5];
+        this.PhoneNumber = param[6];
+        this.Email = param[7];
+    }
 }
